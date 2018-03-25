4e2d573571a3cefb09d0c4563f2db72c5c0bd180
jyuill <john@fig4.com>
2018-03-24 20:02:06 -0700

    more time series - good progress

--- loess-time-series-example.R
+++ loess-time-series-example.R
@@ -33,35 +33,8 @@ summary(smoothed10)
 
 plot1 + geom_line(aes(y=smoothed10, x=economics$date), color='green')
 
-## Evaluate different options
-# define function that returns the SSE
-## FUNCTION DOESN'T WORK !!! ##################################
-calcSSE <- function(x){
-  loessMod <- try(loess(uempmed ~ index, data=economics, span=x), silent=T)
-  res <- try(loessMod$residuals, silent=T)
-  if(class(res)!="try-error"){
-    if((sum(res, na.rm=T) > 0)){
-      sse <- sum(res^2)  
-    }
-  }else{
-    sse <- 99999
-  }
-  return(sse)
-}
+## Predict forward periods with Loess
+## ??
 
-# Run optim to find span that gives min SSE, starting at 0.5
-## optim is general purpose optimizing algorithm -> doesn't work with function above
-optim(par=c(0.5), calcSSE, method="SANN")
-
-## above is meant to compare sse for various spans
-sse10 <- sum(loessMod10$residuals^2)
-sse25 <- sum(loessMod25$residuals^2)
-sse50 <- sum(loessMod50$residuals^2)
-
-mod.compare <- data.frame(model=c("loessMod10", "loessMod25", "loessMod50"), 
-           sse=c(sse10, sse25, sse50))
-mod.compare
-cat("Model with lowest sse:")
-mod.compare %>% filter(sse==min(sse))
 
 
